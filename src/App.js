33e29de73756a893a97eea300e985d71485d74dc
Alan Bischoff <alan_bischoff@Alans-MacBook-Pro-2019.local>
2020-04-23 15:28:59 -0600

    updated page with shop page, and item styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 //Components
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
+const Hats = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-     <Homepage />
+      <Switch>
+        <Route exact path='/' component={ Homepage } />
+        <Route exact path='/shop' component={ ShopPage } />
+        <Route exact path='/hats' component={ Hats } />
+      </Switch>
     </div>
   );
 }
