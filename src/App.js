0baee095a0ee659db57ab7939c0ddf1da65366d4
Alan Bischoff <alan_bischoff@Alans-MacBook-Pro-2019.local>
2020-04-23 16:16:36 -0600

    adding header component and links to home and shop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 //Components
 import Homepage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const Hats = () => (
   <div>
@@ -16,6 +17,7 @@ const Hats = () => (
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={ Homepage } />
         <Route exact path='/shop' component={ ShopPage } />
